cec77737c7c1843f36b4f257a65dad24ec5e84c1
Paul Mueller <paul.mueller@accenture.com>
2024-07-26 08:23:34 +0200

    WIP:Initial commit

---
+++ src/main/java/op/neptun/poc/HelloController.java
@@ -0,0 +1,13 @@
+package op.neptun.poc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Helloooo World!";
+    }
+}
